221ad11fbb0a0a74b15a1dd151c646d52701dda8
Draetus <mauricio199836@hotmail.com>
2017-04-17 15:22:24 -0300

    Setting keyboard and monitor configs

---
+++ .config/i3/.screenlayout/dual_monitor_config.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --off --output DVI-I-1 --mode 1680x1050 --pos 0x0 --rotate normal --output DVI-I-0 --primary --mode 1680x1050 --pos 1680x0 --rotate normal --output DVI-I-3 --off --output DVI-I-2 --off
